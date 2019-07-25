64ea4364fda7fb8f62ba19921c7e01f6e8c3aa20
Vinicius Reis <vinicius.reis@msn.com>
2019-07-25 20:36:24 -0300

    modifica servico de pagamento para openfeing

--- src/main/java/br/com/caelum/eats/pagamento/PagamentoController.java
+++ src/main/java/br/com/caelum/eats/pagamento/PagamentoController.java
@@ -22,6 +22,7 @@ import lombok.AllArgsConstructor;
 class PagamentoController {
 
 	private PagamentoRepository pagamentoRepo;
+	private PedidoRestClient pedidoClient;
 
 	@GetMapping("/{id}")
 	public PagamentoDto detalha(@PathVariable Long id) {
@@ -42,6 +43,10 @@ class PagamentoController {
 		Pagamento pagamento = pagamentoRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException());
 		pagamento.setStatus(Pagamento.Status.CONFIRMADO);
 		pagamentoRepo.save(pagamento);
+		
+		Long pedidoId = pagamento.getPedidoId();
+		pedidoClient.avisaQueFoiPago(pedidoId);
+		
 		return new PagamentoDto(pagamento);
 	}
 
